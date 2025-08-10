918236f0fa0d144a33378c6b462f4b1ca92b74ae
pgyppp <pgy20041122@qq.com>
2025-08-10 13:14:59 +0800

    Update voice_config.py

--- config/voice_config.py
+++ config/voice_config.py
@@ -1,7 +1,7 @@
 VOICE_CONFIG = {
-    'API_KEY': '6ad443396a0dc0143547e7215837e145',   # 替换为你的API密钥
-    'API_SECRET': 'YjI2ZWYxYzhiNzljZThiYzU3Nzg2ODZm',  # 替换为你的API密钥
-    'APP_ID': '25cc212d',   # 替换为你的应用ID
+    'API_KEY': '',   # 替换为你的API密钥
+    'API_SECRET': '',  # 替换为你的API密钥
+    'APP_ID': '',   # 替换为你的应用ID
     'HOST': 'wss://iat-api.xfyun.cn/v2/iat',   # 替换为你的服务地址
     'AUDIO_FORMAT': 'audio/L16;rate=16000',  # 音频格式要求 L16 采样率16000 mp3 16k 单声道
     'LANGUAGE': 'zh_cn',  # 识别语言
